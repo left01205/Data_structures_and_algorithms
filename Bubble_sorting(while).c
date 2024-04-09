a3c8ca277f62bed5ecb660dc58fa7a9d4b010947
left01205 <91931013+left01205@users.noreply.github.com>
2024-04-09 13:42:15 +0530

    Create Bubble_sorting.c
    Sorting an array using bubble sort method in c language

---
+++ Bubble_sorting(while).c
@@ -0,0 +1,43 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int bubble_sort(int arr[],int n )
+{
+    int t;
+    int i=0;
+    
+    while(i<n-1)
+    {
+        for (int j =0;j<n-1-i;j++)
+        {
+            if (arr[j]>arr[j+1])
+            {
+                t=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=t;
+                
+            }
+        
+        }
+        i++;
+    }
+    printf("Elements after being bubble sorted : ");
+    for (int i=0;i<n;i++)
+    {
+        printf("%d ",arr[i]);
+    
+    }
+
+}
+int main() {
+    // Intitalising array 
+    int n;
+    printf("Enter number of elements  in array = ");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter elemnt of array in sequential manner : ");
+    for (int i = 0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    bubble_sort(arr,n);
+}
