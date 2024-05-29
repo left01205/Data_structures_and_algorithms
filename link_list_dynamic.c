b1f785862b99020658d4df56247053250e34d61b
Arnav Jha <91931013+left01205@users.noreply.github.com>
2024-05-29 09:11:04 +0530

    Create link_list_dynamic.c

---
+++ link_list_dynamic.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node 
+{
+    int info;
+    struct node *link;
+};
+
+int main() 
+{
+    int n;
+    printf("Enter the number of nodes: ");
+    scanf("%d", &n);
+
+    struct node *head = NULL, *temp = NULL;
+    for(int i = 0; i < n; i++) 
+    {
+        temp = (struct node *)malloc(sizeof(struct node));
+        printf("Enter the value for node %d: ", i+1);
+        scanf("%d", &temp->info);
+        temp->link = NULL;
+
+        if(head == NULL) 
+        {
+            head = temp;
+        } 
+        else 
+        {
+            struct node *p = head;
+            while(p->link != NULL) 
+            {
+                p = p->link;
+            }
+            p->link = temp;
+        }
+    }
+
+    void print_node(struct node *n) 
+    {
+        while(n != NULL) 
+        {
+            printf("%d ", n->info);
+            n = n->link;
+        }
+        printf("\n");
+    }
+
+    print_node(head);
+
+    return 0;
+}
