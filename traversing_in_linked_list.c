01a9f60d2f3516173e2263dcae84efac9de26cbe
Arnav Jha <91931013+left01205@users.noreply.github.com>
2024-05-30 08:06:06 +0530

    Create traversing_in_linked_list.c

---
+++ traversing_in_linked_list.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node 
+{
+    int info;
+    struct node *link;
+};
+void traverse(struct node *n) 
+{
+    while(n != NULL) 
+    {
+        printf("%d ", n->info);
+        n = n->link;
+    }
+        printf("\n");
+    }
+int main() 
+{
+    int n;
+    printf("Enter the number of nodes: ");
+    scanf("%d", &n);
+
+    struct node *head = NULL, *temp = NULL;
+    for(int i = 0; i < n; i++) 
+    {
+        temp = (struct node *)malloc(sizeof(struct node));
+        printf("Enter the value for node %d: ", i+1);
+        scanf("%d", &temp->info);
+        temp->link = NULL;
+
+        if(head == NULL) 
+        {
+            head = temp;
+        } 
+        else 
+        {
+            struct node *p = head;
+            while(p->link != NULL) 
+            {
+                p = p->link;
+            }
+            p->link = temp;
+        }
+    }
+    traverse(head);
+
+    return 0;
+}
